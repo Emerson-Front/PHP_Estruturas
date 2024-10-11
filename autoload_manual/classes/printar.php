14b5e89cdb9d74589372598c96ca122fc7e9601e
Emerson Teixeira <98964194+Emerson-Front@users.noreply.github.com>
2024-10-11 13:18:21 -0300

    Add files via upload

---
+++ autoload_manual/classes/printar.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace classes;
+
+class Printar
+{
+    public function print()
+    {
+        echo 'Vinda pasta classes';
+    }
+}
---
+++ autoload_manual/utilidades/printar.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace utilidades;
+
+class Printar
+{
+    public function print()
+    {
+        echo 'Vinda pasta utilidades';
+    }
+}
